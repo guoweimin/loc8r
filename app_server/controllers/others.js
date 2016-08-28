26dcf3dccc60c0b19c151ae3174b64ca3fa0b3d1
Weimin Guo <guo.weimin@outlook.com>
2016-08-28 15:02:11 +0800

    Learning MVC
    Move data from views into controllers;
    Use the "http://api.map.baidu.com/staticimge" api instead of
    "http://maps.googleapis.com/maps/api/staticmap" api.

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,7 @@
 /* GET 'about' page */
 module.exports.about = function(req, res) {
-  res.render('generic-text', { title: 'Aboout'});
+  res.render('generic-text', {
+    title: 'Aboout Loc8r',
+    content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\nYou’ll probably want to add some additional lines in there, so that the page looks like it has real content. Notice that the lines starting with the pipe character (|) can contain HTML tags if you want them to. Figure 4.13 shows how this might look in the browser with a bit more content in it.\n\nAnd that’s the last one of the four pages we need for the static site. You can now push this up to Heroku and have people visit the URL and click around. If you’ve forgotten how to do this, the following code snippet shows the terminal commands you need, assuming you’ve already set up Heroku. In terminal, you need to be in the root folder of the application.'
+  });
 };
